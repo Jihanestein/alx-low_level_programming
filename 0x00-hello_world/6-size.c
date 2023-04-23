1ea3bbaa868998930cd4dc6ea92522d8d4076805
jihane <jihanebhsbhs@gmail.com>
2023-04-23 11:35:12 -0700

    task6 cpro

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int integerType;
 float floatType;
 double doubleType;
 char charType;
-printf("Size of a char: %lu bytes\n", sizeof(char));
-printf("Size of an int: %lu bytes\n", sizeof(int));
-printf("Size of a long: %lu byte\n", sizeof(long int));
-printf("Size of a long long int: %lu byte\n", sizeof(long long int));
-printf("Size of a float: %lu byte\n", sizeof(float));
+	printf("Size of a char: %lu bytes\n", sizeof(char));
+	printf("Size of an int: %lu bytes\n", sizeof(int));
+	printf("Size of a long: %lu byte\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte\n", sizeof(long long int));
+	printf("Size of a float: %lu byte\n", sizeof(float));
 return (0);
 }
