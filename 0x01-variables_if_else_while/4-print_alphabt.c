6ff38e4ce836b15ec52cc3fce5b69d4ba0926772
jihane <jihanebhsbhs@gmail.com>
2023-04-23 19:03:39 -0700

    task4 var

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 97;
+
+	while (a <= 122)
+	{
+		if (a == 101 || a == 113)
+		{
+			continue;
+			a++;
+		}
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
