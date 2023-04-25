4a64d69a960b9893b5b87605ba68670a93b3f65b
jihane <jihanebhsbhs@gmail.com>
2023-04-25 14:34:18 -0700

    task1 loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Description   function that prints the alphabet
+ * Return: void.
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
