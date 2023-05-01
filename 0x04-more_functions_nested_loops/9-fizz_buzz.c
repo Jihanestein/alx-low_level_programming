928986a5fd1ef03d34bf5a837aa24679fdce158c
jihane <jihanebhsbhs@gmail.com>
2023-04-30 17:58:41 -0700

    taks4 lop4

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,17 +15,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-	if (i % 3 == 0 && i % 5 != 0)
+	if ((i % 3 == 0) && (i % 5 != 0))
 	{
 		printf("Fizz");
 		putchar(' ');
 	}
-	else if (i % 5 == 0 && i % 3 != 0)
+	else if ((i % 5 == 0) && (i % 3 != 0))
 	{
 		printf("Buzz");
 		putchar(' ');
 	}
-	else if (i % 5 == 0 && i % 3 == 0)
+	else
 	{
 		printf("FizzBuzz");
 		putchar(' ');
