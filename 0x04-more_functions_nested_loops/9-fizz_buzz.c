41a5e64b16d297d5ba03146163edbbff6aa85d37
jihane <jihanebhsbhs@gmail.com>
2023-04-30 17:53:01 -0700

    task 9 lop4

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,26 +15,26 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
-		{
-			printf("Fizz");
-			putchar(' ');
-		}
-		else if (i % 5 == 0 && i % 3 != 0)
-		{
-			printf("Buzz");
-			putchar(' ');
-		}
-		else if (i % 5 == 0 && i % 3 == 0)
-		{
-			printf("FizzBuzz");
-			putchar(' ');
-		}
-		else
-		{
-			printf("%d", i);
-			putchar(' ');
-		}
+	if (i % 3 == 0 && i % 5 != 0)
+	{
+		printf("Fizz");
+		putchar(' ');
+	}
+	else if (i % 5 == 0 && i % 3 != 0)
+	{
+		printf("Buzz");
+		putchar(' ');
+	}
+	else if (i % 5 == 0 && i % 3 == 0)
+	{
+		printf("FizzBuzz");
+		putchar(' ');
+	}
+	else
+	{
+		printf("%d", i);
+		putchar(' ');
+	}
 		putchar('\n');
 	}
 }
