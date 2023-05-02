449c50e4483caace8e468f640fee031c6707d1b7
jihane <jihanebhsbhs@gmail.com>
2023-05-02 11:36:36 -0700

    task0 pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - fctn that point int as para and updates it values to 98
+ *
+ * @n: input
+ * Return: value of n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
