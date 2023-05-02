32b973ad94bd711debc6902e33825f60597cf66d
jihane <jihanebhsbhs@gmail.com>
2023-05-02 12:46:46 -0700

    task 4 pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - function that print a string a reverse it
+ * @s: string
+ *
+ * Return: string reversed
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	for (i = len; i > 0; i--)
+	{
+	putchar(*s);
+	s--;
+	}
+}
