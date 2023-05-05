f3724e7f76913018a790ecf03cfa7e8574085aa5
jihane <jihanebhsbhs@gmail.com>
2023-05-04 17:12:13 -0700

    task 7 leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *leet - fctn that encode a string into 1337
+ * @n: input
+ * Return: n
+ */
+
+char *leet(char *n)
+{
+	int a;
+	int b;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (a = 0; n[a] != '\0'; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (n[a] == s1[b])
+			{
+				n[a] = s2[b];
+			}
+		}
+	}
+	return (n);
+}
