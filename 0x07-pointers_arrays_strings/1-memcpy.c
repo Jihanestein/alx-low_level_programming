1b4e1ada700e1b358f4fc66dd36a518a1bcfd22d
jihane <jihanebhsbhs@gmail.com>
2023-05-08 14:44:55 -0700

    task 1 poinetrs

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,10 +5,19 @@
  * *_memcpy - function that copy n bytes fro src to dest
  * @src: memory area
  * @dest: memory area
- *
+ * @n: bytes
  * Return: copied memory area
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	int a = 0;
+	int c = 0;
 
+	for (; a < c; a++)
+	{
+		dest[a] = src[a];
+		n--;
+	}
+	return (dest);
+}
