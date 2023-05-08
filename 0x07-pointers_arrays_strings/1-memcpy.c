e79d7a117ed583f02ac4dde053ecd9da60a858d9
jihane <jihanebhsbhs@gmail.com>
2023-05-08 14:32:57 -0700

    Task 0 pointers

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_memcpy - function that copy n bytes fro src to dest
+ * @src: memory area
+ * @dest: memory area
+ *
+ * Return: copied memory area
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Poinrers task
