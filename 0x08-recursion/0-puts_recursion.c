6bd5ffc1742c1473475060a4b11ce92ec11cc45e
jihane <jihanebhsbhs@gmail.com>
2023-05-10 14:14:44 -0700

    task 0 recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function that prints a string followed by ne line
+ * @s: input
+ * Return:string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
