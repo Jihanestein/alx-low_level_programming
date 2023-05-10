2fc118a9eefdb1e982e013ebf25e04b1a52ea129
jihane <jihanebhsbhs@gmail.com>
2023-05-10 16:10:03 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - fct that return 1 if n is prime number
+ * @n: number
+ * @a: int
+ * Return: 1 if prime number otherwise 0
+ */
+
+int is_prime_number(int n, int a)
+{
+	if (a == 1)
+	{
+		return (1);
+	}
+	if (n % a == 0 && a > 0)
+	{
+		return (0);
+	}
+	
+	return (is_prime_number(n, a - 1));
+}
