0d321952d80a46144c358fac6fa30876b1f4ce9c
jihane <jihanebhsbhs@gmail.com>
2023-05-11 14:49:30 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,8 +11,11 @@
 int _sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
-		return (1);
+		return (-1);
 	if (i * i == n)
 		return (i);
-	return (_sqrt_recursion(n, i - 1));
+	return (_sqrt_recursion(n, i + 1));
+	if (n < 0)
+		return (-1);
+	return (_sqrt_recursion(n, 0));
 }
