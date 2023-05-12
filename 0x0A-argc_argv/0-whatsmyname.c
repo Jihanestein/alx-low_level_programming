00b904e33eb92867afefcc5c9e637cf515a01804
jihane <jihanebhsbhs@gmail.com>
2023-05-12 14:54:01 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - function that print name
+* @argv: array of the arguments
+* @argc: number of arguments 
+* Return: Always (0) sucess
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
