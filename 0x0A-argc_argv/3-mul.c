d80bd8c660eee642712f3740482178589be78e35
jihane <jihanebhsbhs@gmail.com>
2023-05-13 13:52:08 -0700

    task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *main - funtion that multiple two numbers
+  *atoi - convert string to intger
+  *@argc: number of arguments
+  *@argv: array of arguments
+  *
+  *Return: 0 (always sucess)
+  */
+
+int main(int argc, char *argv[])
+{
+	int a = 0;
+	int b = 0;
+
+	if (argc < 3 || argc > 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
