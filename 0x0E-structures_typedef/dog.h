1859beab8a20198f69186fc95dedd48acd65891f
jihane <jihanebhsbhs@gmail.com>
2023-05-22 15:44:06 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,25 +19,15 @@ struct dog
 };
 
 /**
-  *main - dog information
-  *@Dog.name: poppy
-  *@Dog.age: number
-  *@Dog.owner: owner name
-  *
-  *Return: 0 (alwyas success)
-  */
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
 
-int main(void)
-{
-	struct dog myDog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
-	Dog.name = "poppy";
-	Dog.age = 3.5;
-	Dog.owner = "bob";
-
-	printf("%s\n", Dog.name);
-	printf("%.1f\n", Dog.age);
-	printf("%s\n", Dog.owner);
-
-	return (0);
-}
+#endif
